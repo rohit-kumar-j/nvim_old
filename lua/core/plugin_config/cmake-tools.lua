60644b838adf17148a0344ec37fdc3e3688a7f5a
RKJ <37873142+rohit-kumar-j@users.noreply.github.com>
2023-04-21 00:16:02 +0530

    handle breaking changes to main repo

--- lua/core/plugin_config/cmake-tools.lua
+++ lua/core/plugin_config/cmake-tools.lua
@@ -1,26 +1,26 @@
 -- require("cmake-tools").setup({})
 require("cmake-tools").setup {
 
-    cmake_command = "cmake",      -- cmake command path
-    cmake_build_directory = "build", -- cmake generate directory
-    -- cmake_build_directory_prefix = "cmake_build_", -- when cmake_build_directory is "", this option will be activated
-    -- cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" }, -- it will be activated when invoke `cmake.generate`
-    cmake_build_options = {},           -- it will be activated when invoke `cmake.build`
-    cmake_console_position = "belowright", -- "bottom", "top"
-    cmake_console_size = 10,
-    cmake_show_console = "always",      -- "always", "only_on_error"
-    cmake_focus_on_console = false,     -- true, false
-    cmake_variants_message = {
-        short = { show = true },
-        long = { show = true, max_length = 40 }
-    },
-    cmake_dap_configuration = {
-        name = "cpp",
-        type = "codelldb",
-        request = "launch",
-        stopOnEntry = false,
-        runInTerminal = true,
-        console = "integratedTerminal",
-    },
+	cmake_command = "cmake",                                       -- cmake command path
+	cmake_build_directory = "build",                               -- cmake generate directory
+	-- cmake_build_directory_prefix = "cmake_build_", -- when cmake_build_directory is "", this option will be activated
+	cmake_generate_options = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" }, -- it will be activated when invoke `cmake.generate`
+	cmake_build_options = {},                                      -- it will be activated when invoke `cmake.build`
+	cmake_console_position = "belowright",                         -- "bottom", "top"
+	cmake_console_size = 20,
+	cmake_show_console = "always",                                 -- "always", "only_on_error"
+	cmake_focus_on_console = false,                                -- true, false
+	cmake_variants_message = {
+		short = { show = true },
+		long = { show = true, max_length = 40 }
+	},
+	cmake_dap_configuration = {
+		name = "cpp",
+		type = "codelldb",
+		request = "launch",
+		stopOnEntry = false,
+		runInTerminal = true,
+		console = "integratedTerminal",
+	},
 
 }
