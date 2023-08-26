ce26cb5057139bbb66d0090f55ae1cdb251312ab
RKJ <37873142+rohit-kumar-j@users.noreply.github.com>
2023-08-26 11:08:56 +0530

    Configured Vimtex for Win32 and Linux

--- ftplugin/tex.lua
+++ ftplugin/tex.lua
@@ -9,6 +9,19 @@ function! ZathuraHook() dict abort
   silent call system('xdotool windowactivate ' . self.xwin_id . ' --sync')
   silent call system('xdotool windowraise ' . self.xwin_id)
 endfunction
-
-let g:vimtex_view_general_viewer= 'ZathuraHook'
 ]])
+
+local is_macunix = vim.fn.has("macunix")
+local is_win32 = vim.fn.has("win32")
+local is_wsl = vim.fn.has("wsl")
+
+if (is_macunix == 1) then
+    vim.g.vimtex_view_general_viewer = "ZathuraHook"
+end
+if (is_win32 == 1) then
+    vim.g.vimtex_view_general_viewer = "SumatraPDF"
+    vim.g.vimtex_view_general_options = [[-reuse-instance -forward-search @tex @line @pdf]]
+end
+if (is_wsl == 1) then
+    vim.g.vimtex_view_general_viewer = "ZathuraHook"
+end
